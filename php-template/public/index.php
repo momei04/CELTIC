81ec728571b8963c6712ceecfd747d1e44cc8aa6
Moritz Meier <moritzmeiermac@gmail.com>
2023-10-04 13:17:46 +0200

    initial commit

---
+++ php-template/public/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.8.1/js/foundation.min.js" integrity="sha512-HugKDgJVjWdi/RfSbpjwOPqBhO2AQ6TuETbq7eGGutNulCWxZuERGwK9RutFQ/rbogsqpmfOtqJpFy2Y4AeJcQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;600;900&family=Titillium+Web:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="template/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <nav>
+        <img src="https://www.weareboxfish.com/wp-content/uploads/2017/09/celticLogo.png" alt="" class="logo">
+        <ul>
+            <li><a href="#">Transfers</a></li>
+            <li><a href="#">Matches</a></li>
+            <li><a href="#">Line up</a></li>
+        </ul>
+    </nav>
+</header>
+
+<!--Main Content-->
+<div class="header_container">
+    <h1 class="header_container-title">Celtic</h1>
+</div>
+<div class="content-container">
+    <!--Slider-->
+    <div class="slider">
+        <div class="slide">
+            <img src="https://www.lequipe.fr/_medias/img-photo-jpg/kyogo-furuhashi-a-droite-a-inscrit-un-double-ce-samedi-contre-les-rangers-reuters/1500000001771142/0:0,1998:1332-828-552-75/2682b.jpg" alt="">
+        </div>
+        <div class="slide">
+            <img src="https://abseits.at/wp-content/uploads/Celtic-Glasgow1.jpg" alt="">
+        </div>
+        <div class="slide">
+            <img src="https://imagenes.elpais.com/resizer/7BzUSgVtyFoWltxWYHhwM3zPTnA=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/E25ULTIKLY2DKNPXY2NPYNDIKQ.jpg" alt="">
+        </div>
+        <div class="slide">
+            <img src="https://tuttocalciomercato24.com/wp-content/uploads/2021/12/ryan-christie-celtic-scozia-premier-europa-league-1.jpg" alt="">
+        </div>
+        <div class="slide">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Celtic_Park3.jpg" alt="">
+        </div>
+        <div class="slide">
+            <img src="https://cdn.thecelticblog.com/wp-content/uploads/2022/08/2022-07-31T170749Z_942239235_UP1EI7V1BKZVK_RTRMADP_3_SOCCER-SCOTLAND-CEL-ABE-REPORT-scaled.jpg?width=1201" alt="">
+        </div>
+        <div class="slide">
+            <img src="https://www.absolut-sport.com/fileadmin/_processed_/a/1/csm_Celtic_Park_-_Celtic_Glasgow_fe8049ef3d.jpg?_=1498568518" alt="">
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
